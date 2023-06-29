cd250ecadb68da340815a7a18f9bb68ea4ada63b
SwetZadafiya <136707833+SwetZadafiya@users.noreply.github.com>
2023-06-29 17:04:16 +0530

    Update swet2_2.c

--- swet2_2.c
+++ swet2_2.c
@@ -5,24 +5,24 @@
 #include<stdio.h>
 #include<conio.h>
 
-int main (){
+void main (){
 
 float bill,a,f;
 
-printf("-------------------------------------\n");
-printf("-----------ELECTRICITY BILL----------\n");
-printf("-------------------------------------\n\n");
+printf("======================================\n");
+printf("=-=-=-=-=-=ELECTRICITY BILL=-=-=-=-=-=\n");
+printf("======================================\n\n");
 
-printf("-------------------------------------\n");
+printf("************************************\n");
 printf("       charges of per unit ");
 printf("For the first 50 units Rs. 0.50/unit\nFor the next 100 units Rs. 0.75/unit\nFor the next 100 units Rs. 1.20/unit\n");
 printf("For units above 250 Rs. 1.50/unit\n");
-printf("-------------------------------------\n\n");
+printf("************************************\n\n");
 
 printf("*************************************\n");
 printf("      Enter electricity units:  ");
 scanf("%f",&bill);
-printf("\n*************************************\n\n");
+printf("\n***********************************\n\n");
 
 
 
@@ -32,29 +32,23 @@ if(bill<=50){
              printf("Electricity Bill = Rs.  %.1f",f);
              
             }else if(bill<=150){
-                                a=(25 + (bill - 50)*0.75);
-                                f=a +a*0.2;  
-                                printf("Electricity Bill = Rs.  %0.1f",f);
+                     a=(25 + (bill - 50)*0.75);
+                     f=a +a*0.2;  
+                     printf("Electricity Bill = Rs.  %0.1f",f);
             }else if(bill<=250){
-                                a=(25 + 75 +(bill-150)*1.20);
-                                f=a +a*0.2;
-                                printf("Electricity bill = Rs.  %.1f",f);
+                     a=(25 + 75 +(bill-150)*1.20);
+                     f=a +a*0.2;
+                     printf("Electricity bill = Rs.  %.1f",f);
 
             }else{
-                  a=(25 + 75 + 120 + (bill - 250 )*1.50);
-                  f=a +a*0.2;
-                  printf("Electricity Bill = Rs.  %.1f",f);
+              a=(25 + 75 + 120 + (bill - 250 )*1.50);
+              f=a +a*0.2;
+              printf("Electricity Bill = Rs.  %.1f",f);
             }
 
-
-
-return 0;
 getch();    
 }
-
-/*Input:
-       Enter electricity units: 435
-Output: 
-       Electricity Bill = Rs. 597.0
+/*Input : Enter electricity units: 435
+Output : Electricity Bill = Rs. 597.0
 */
 
